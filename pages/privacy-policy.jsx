0b83353d61c3229d12e884a0b3b78e1d045fa1db
Renan Sigolo <renan.sigolo@gmail.com>
2021-09-23 15:35:41 +1000

    feat: add privacy policy

---
+++ pages/privacy-policy.jsx
@@ -0,0 +1,52 @@
+const PrivacyPolicy = () => {
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-12">
+        {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+        <div className="max-w-3xl mx-auto">
+          <h1 className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+            Privacy Policy
+          </h1>
+          <p className="mt-5 mx-auto text-xl text-gray-800">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            perspiciatis nam maiores rem autem reprehenderit expedita porro
+            eaque ipsa. Corrupti dolorum consequuntur doloremque totam! Dolores
+            mollitia sit earum voluptatum expedita.
+          </p>
+          <p className="mt-5 mx-auto text-xl text-gray-800">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            perspiciatis nam maiores rem autem reprehenderit expedita porro
+            eaque ipsa. Corrupti dolorum consequuntur doloremque totam! Dolores
+            mollitia sit earum voluptatum expedita.
+          </p>
+          <p className="mt-5 mx-auto text-xl text-gray-800">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            perspiciatis nam maiores rem autem reprehenderit expedita porro
+            eaque ipsa. Corrupti dolorum consequuntur doloremque totam! Dolores
+            mollitia sit earum voluptatum expedita.
+          </p>
+          <p className="mt-5 mx-auto text-xl text-gray-800">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            perspiciatis nam maiores rem autem reprehenderit expedita porro
+            eaque ipsa. Corrupti dolorum consequuntur doloremque totam! Dolores
+            mollitia sit earum voluptatum expedita.
+          </p>
+          <p className="mt-5 mx-auto text-xl text-gray-800">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            perspiciatis nam maiores rem autem reprehenderit expedita porro
+            eaque ipsa. Corrupti dolorum consequuntur doloremque totam! Dolores
+            mollitia sit earum voluptatum expedita.
+          </p>
+          <p className="mt-5 mx-auto text-xl text-gray-800">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            perspiciatis nam maiores rem autem reprehenderit expedita porro
+            eaque ipsa. Corrupti dolorum consequuntur doloremque totam! Dolores
+            mollitia sit earum voluptatum expedita.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PrivacyPolicy;
